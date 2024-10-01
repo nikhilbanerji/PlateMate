93edf7bbaa02b756b54cbfaba9428aafe1bb9633
nikhilbanerji <nb60@rice.edu>
2024-10-01 17:44:15 -0500

    Initial project setup

---
+++ src/main/java/com/nbsa/plateMate/PlateMateApplication.java
@@ -0,0 +1,13 @@
+package com.nbsa.plateMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nbsa/plateMate/PlateMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbsa.plateMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
